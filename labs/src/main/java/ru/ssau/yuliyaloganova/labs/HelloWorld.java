8e9bf281573da479bb7cebe039c0b9648dd50f49
Loganova <yuliya.shahova18@gmail.com>
2023-09-24 17:05:51 +0400

    Добавлен первый Java-класс

--- labs/src/main/java/ru/ssau/yuliyaloganova/labs/HelloWorld.java
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/HelloWorld.java
@@ -2,6 +2,6 @@ package ru.ssau.yuliyaloganova.labs;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
