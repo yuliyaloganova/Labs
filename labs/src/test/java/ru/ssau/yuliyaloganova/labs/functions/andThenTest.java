f0ab6d4ff03a534410f8558d572340921fdf9c82
Loganova <yuliya.shahova18@gmail.com>
2023-10-09 22:08:01 +0400

    исправление ошибок

--- labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/andThenTest.java
+++ labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/andThenTest.java
@@ -1,22 +1,35 @@
 package ru.ssau.yuliyaloganova.labs.functions;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class andThenTest {
     private static final double delta = 1e-15;
 
-    public void TestApply(){
+    @Test
+    public void TestApply() {
         MathFunction f = x -> x * 3;
         MathFunction g = x -> x + 2;
         MathFunction h = x -> Math.pow(x, 4);
 
         MathFunction compFunc1 = f.andThen(g).andThen(h);
         assertEquals(7209, compFunc1.apply(7), delta);
+    }
+
+    public void TestApply2() {
+        MathFunction f = x -> x * 3;
+        MathFunction g = x -> x + 2;
+        MathFunction h = x -> Math.pow(x, 4);
 
         MathFunction compFunc2 = g.andThen(h).andThen(f);
         assertEquals(20738, compFunc2.apply(4), delta);
-
+    }
+    public void TestApply3() {
+        MathFunction f = x -> x * 3;
+        MathFunction g = x -> x + 2;
+        MathFunction h = x -> Math.pow(x, 4);
         MathFunction compFunc3 = h.andThen(f).andThen(g);
         assertEquals(20736, compFunc3.apply(2), delta);
     }
