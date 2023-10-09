cad81fa2efaa2fb2f5188bbb955d9e38dff04a27
Polikarpova <polikarpovaarina@gmail.com>
2023-10-09 14:33:40 +0400

    Создание интерфейса TabulatedFunction, расширяющий интерфейс MathFunction

---
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/functions/TabulatedFunction.java
@@ -0,0 +1,12 @@
+package ru.ssau.yuliyaloganova.labs.functions;
+
+public interface TabulatedFunction extends MathFunction {
+    int getCount(); //Метод получения количества табулированных значений
+    double getX(int index); //Метод, получающий значение аргумента x по номеру индекса
+    double getY(int index); //Метод, получающий значение y по номеру индекса
+    void setY(int index, double value); //Метод, задающий значение y по номеру индекса
+    int indexOfX(double x); //Метод, возвращающий индекс аргумента x
+    int indexOfY(double y); //Метод, возвращающий индекс первого вхождения значения y
+    double leftBound(); //Метод, возвращающий самый левый x
+    double rightBound(); //Метод, возвращающий самый правый x
+}
