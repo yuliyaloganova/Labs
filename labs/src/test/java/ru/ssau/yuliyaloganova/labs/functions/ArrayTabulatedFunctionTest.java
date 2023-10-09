6e43f8810c5c5805acc03a6770e205f1c3d19a2b
Loganova <yuliya.shahova18@gmail.com>
2023-10-09 21:36:19 +0400

    тестирование всех методов класса ArrayTabulatedFunction

--- labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/ArrayTabulatedFunctionTest.java
+++ labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/ArrayTabulatedFunctionTest.java
@@ -17,21 +17,18 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testGetX() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(2.0, function.getX(1), 0.0);
     }
 
     @Test
     public void testGetY() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(5.0, function.getY(1), 0.0);
     }
 
     @Test
     public void testSetY() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         function.setY(1, 10.0);
         Assert.assertEquals(10.0, function.getY(1), 0.0);
@@ -39,63 +36,54 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testIndexOfX() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(1, function.indexOfX(2.0));
     }
 
     @Test
     public void testIndexOfY() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(1, function.indexOfY(5.0));
     }
 
     @Test
     public void testLeftBound() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(1.0, function.leftBound(), 0.0);
     }
 
     @Test
     public void testRightBound() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(3.0, function.rightBound(), 0.0);
     }
 
     @Test
     public void testInterpolate() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(4.5, function.interpolate(1.5, 1), 0.0);
     }
 
     @Test
     public void testFloorIndexOfX() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(1, function.floorIndexOfX(2.5));
     }
 
     @Test
     public void testExtrapolateLeft() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(3.5, function.extrapolateLeft(0.5), 0.0);
     }
 
     @Test
     public void testExtrapolateRight() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(6.5, function.extrapolateRight(3.5), 0.0);
     }
 
     @Test
     public void testInterpolateSingle() {
-        // Assuming the function is already created
         ArrayTabulatedFunction function = createFunction();
         Assert.assertEquals(5.5, function.interpolate(2.5), 0.0);
     }
