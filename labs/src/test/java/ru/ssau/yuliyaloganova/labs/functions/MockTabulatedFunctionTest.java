e27cfe81493344932f777f48170b4889b774153e
Loganova <yuliya.shahova18@gmail.com>
2023-10-09 16:16:04 +0400

    тест через mock objec

---
+++ labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/MockTabulatedFunctionTest.java
@@ -0,0 +1,25 @@
+package ru.ssau.yuliyaloganova.labs.functions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+public class MockTabulatedFunctionTest {
+
+    private static final double EPSILON = 1e-10;
+    @Test
+    public void TestInterpolate() {
+        AbstractTabulatedFunction function = new MockTabulatedFunction(-1.0, 1.0, -1.0, 1.0);
+        assertEquals(0.0, function.interpolate(0.0, 0), EPSILON);
+        assertEquals(-0.5, function.interpolate(-0.5, 0), EPSILON);
+        assertEquals(0.5, function.interpolate(0.5, 0), EPSILON);
+    }
+
+    @Test
+    public void TestApply() {
+        AbstractTabulatedFunction function = new MockTabulatedFunction(-1.0, 1.0, -1.0, 1.0);
+        assertEquals(1.0, function.apply(0.0), EPSILON);
+        assertEquals(-1.0, function.apply(-2.0), EPSILON);
+        assertEquals(1.0, function.apply(2.0), EPSILON);
+    }
+
+}
