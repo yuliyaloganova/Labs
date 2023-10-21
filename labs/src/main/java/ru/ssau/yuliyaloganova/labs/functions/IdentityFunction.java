c0845515d7e10cfdebe9236849c0c83329782351
Loganova <yuliya.shahova18@gmail.com>
2023-10-21 19:01:24 +0400

    переопределенные методы toString(), equals(), hashCode() и clone() в классе IdentityFunction

--- labs/src/main/java/ru/ssau/yuliyaloganova/labs/functions/IdentityFunction.java
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/functions/IdentityFunction.java
@@ -1,7 +1,27 @@
 package ru.ssau.yuliyaloganova.labs.functions;
 
-public class IdentityFunction implements MathFunction {
+public class IdentityFunction implements MathFunction, Cloneable {
     public double apply(double x) {
         return x;
+    }
+
+    @Override
+    public String toString(){
+        return "This is the IdentityFunction class";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return this.getClass() == o.getClass();
+    }
+
+    @Override
+    public int hashCode() {
+        return 31; // простейшая реализация хэш-функции
+    }
+
+    @Override
+    public Object clone(){
+        return new IdentityFunction();
     }
 }
