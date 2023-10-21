c8662e88a3029075808a6178147bcf4e66ef670c
Loganova <yuliya.shahova18@gmail.com>
2023-10-21 21:23:02 +0400

    тесты для методов из лр 3.1

--- labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/IdentityFunctionTest.java
+++ labs/src/test/java/ru/ssau/yuliyaloganova/labs/functions/IdentityFunctionTest.java
@@ -13,4 +13,46 @@ class IdentityFunctionTest {
         assertEquals(-1.0, f.apply(-1.0), 0.0);
     }
 
+    @Test
+    public void testToString() {
+        IdentityFunction func = new IdentityFunction();
+        String expected = "This is the IdentityFunction class";
+        assertEquals(expected, func.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        IdentityFunction func1 = new IdentityFunction();
+        IdentityFunction func2 = new IdentityFunction();
+        IdentityFunction diffunc = new IdentityFunction();
+
+        assertTrue(func1.equals(func1)); // объект равен самому себе
+        assertTrue(func1.equals(func2)); // два объекта IdentityFunction равны
+        assertFalse(func1.equals(diffunc)); // объекты IdentityFunction не равны
+        assertFalse(func1.equals(null)); // объект не равен null
+        assertFalse(func1.equals("Not an IdentityFunction")); // объект не равен объекту другого класса
+    }
+
+    @Test
+    public void testHashCode() {
+        IdentityFunction identityFunction1 = new IdentityFunction();
+        IdentityFunction identityFunction2 = new IdentityFunction();
+
+        // Проверяем, что хэш-коды равны
+        assertEquals(identityFunction1.hashCode(), identityFunction2.hashCode());
+    }
+
+    @Test
+    public void testClone() throws CloneNotSupportedException {
+        IdentityFunction identityFunction1 = new IdentityFunction();
+
+        // Создаем клон объекта
+        IdentityFunction identityFunction2 = (IdentityFunction) identityFunction1.clone();
+
+        // Проверяем, что клон и оригинальный объект не являются одним и тем же объектом
+        assertNotSame(identityFunction1, identityFunction2);
+
+        // Проверяем, что клон и оригинальный объект равны
+        assertEquals(identityFunction1, identityFunction2);
+    }
 }
