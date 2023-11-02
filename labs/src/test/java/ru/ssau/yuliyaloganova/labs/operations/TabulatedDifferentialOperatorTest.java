dcbb23380dbbaad74647dc8d093538afa0d34203
Polikarpova <polikarpovaarina@gmail.com>
2023-11-03 00:57:48 +0400

    Покрытие тестами TabulatedDifferentialOperator

---
+++ labs/src/test/java/ru/ssau/yuliyaloganova/labs/operations/TabulatedDifferentialOperatorTest.java
@@ -0,0 +1,38 @@
+package ru.ssau.yuliyaloganova.labs.operations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import ru.ssau.yuliyaloganova.labs.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.yuliyaloganova.labs.functions.ArrayTabulatedFunction;
+import ru.ssau.yuliyaloganova.labs.functions.TabulatedFunction;
+
+
+class TabulatedDifferentialOperatorTest {
+    double[] xValue = {1, 2, 3, 8};
+    double[] yValue = {6, 7, 9, 15};
+
+    @Test
+    void testsetFactory() {
+        LinkedListTabulatedFunctionFactory fact = new LinkedListTabulatedFunctionFactory();
+        TabulatedDifferentialOperator operation = new TabulatedDifferentialOperator();
+        operation.setFactory(fact);
+
+        assertSame(operation.getFactory().getClass(), fact.getClass());
+
+    }
+
+    @Test
+    void testderive() {
+        LinkedListTabulatedFunctionFactory fact = new LinkedListTabulatedFunctionFactory();
+        TabulatedDifferentialOperator operation = new TabulatedDifferentialOperator(fact);
+        ArrayTabulatedFunction func = new ArrayTabulatedFunction(xValue, yValue);
+        TabulatedFunction differential_func = operation.derive(func);
+        assertEquals(1, differential_func.getY(0));
+        assertEquals(2, differential_func.getY(1));
+        assertEquals(1.2, differential_func.getY(2));
+        assertEquals(1.2, differential_func.getY(3));
+    }
+
+
+}
