0c665a32b8ff35c4d99c76d8d38be4c5d684f917
Loganova <yuliya.shahova18@gmail.com>
2023-11-19 20:38:50 +0400

    readTabulatedFunction

--- labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
@@ -2,10 +2,15 @@ package ru.ssau.yuliyaloganova.labs.io;
 
 import ru.ssau.yuliyaloganova.labs.functions.Point;
 import ru.ssau.yuliyaloganova.labs.functions.TabulatedFunction;
+import ru.ssau.yuliyaloganova.labs.functions.factory.TabulatedFunctionFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.io.IOException;
 
 final class FunctionsIO {
     private FunctionsIO(){
@@ -20,5 +25,26 @@ final class FunctionsIO {
             printWriter.printf("%f %f\n", point.x, point.y);
         }
         printWriter.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int cnt = Integer.parseInt(reader.readLine());//извлечение целочисленного значения из первой строки
+        double[] xValues = new double[cnt];
+        double[] yValues = new double[cnt];
+
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));//объект форматирования дробных чисел
+
+        for (int i = 0; i < cnt; i++) { //пробегаемся cnt раз
+            String line = reader.readLine();
+            String[] values = line.split(" ");//разбиение строки по пробелу
+            try {
+                xValues[i] = numberFormat.parse(values[0]).doubleValue();
+                yValues[i] = numberFormat.parse(values[1]).doubleValue();
+            } catch (ParseException e) {
+                throw new IOException(e);
+            }
+        }
+
+        return factory.create(xValues, yValues);
     }
 }
