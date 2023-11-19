0a238d218d033352f36312bfb335486a587ce2cf
Loganova <yuliya.shahova18@gmail.com>
2023-11-19 12:47:17 +0400

    writeTabulatedFunction

--- labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
@@ -1,7 +1,23 @@
 package ru.ssau.yuliyaloganova.labs.io;
 
+import ru.ssau.yuliyaloganova.labs.functions.Point;
+import ru.ssau.yuliyaloganova.labs.functions.TabulatedFunction;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 final class FunctionsIO {
     private FunctionsIO(){
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }
