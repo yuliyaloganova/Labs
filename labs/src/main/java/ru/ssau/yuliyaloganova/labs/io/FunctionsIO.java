cf43243c0a7981ac59d414b2b8281c892fdb1e98
Polikarpova <polikarpovaarina@gmail.com>
2023-11-19 21:23:44 +0400

    добавление статического метода:  writeTabulatedFunction

--- labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/io/FunctionsIO.java
@@ -6,6 +6,8 @@ import ru.ssau.yuliyaloganova.labs.functions.TabulatedFunction;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 
 final class FunctionsIO {
     private FunctionsIO(){
@@ -20,5 +22,17 @@ final class FunctionsIO {
             printWriter.printf("%f %f\n", point.x, point.y);
         }
         printWriter.flush();
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+
+        dataOutputStream.flush();
     }
 }
