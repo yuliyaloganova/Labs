3070380f55623acc255bfcfe749e199182c036a1
Loganova <yuliya.shahova18@gmail.com>
2023-12-01 17:43:25 +0400

    MultiplyingTaskExecutor

---
+++ labs/src/main/java/ru/ssau/yuliyaloganova/labs/functions/UnitFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.yuliyaloganova.labs.functions;
+
+public class UnitFunction extends ConstantFunction {
+    public UnitFunction() {
+        super(1);//вызывает конструктор суперкласса со значением 1
+    }
+}
